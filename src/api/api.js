935eacac80bd1ef38c0ba6dc8db7ba975111be81
Aleksey <rusal@bk.ru>
2022-08-29 22:06:56 +0300

    add activity Co-authored-by: ToxicCreator <ToxicCreator@users.noreply.github.com>

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+﻿export default class API {
+    // Получает сообщение по id
+    static async getDash(id) {
+        const url = "http://localhost:5000"
+        const resp = await fetch(`${url}/dash/${id}`);
+        const json = await resp.json();
+        return json;
+    }
+}
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,46 +1,80 @@
+import {useState, useEffect} from 'react';
 import LiveStream from "components/live-stream/LiveStream";
 import DetectionCard from "components/detection-card/DetectionCard";
 import HistoryCard from "components/history-card/HistoryCard";
 import styles from "./MainLayout.module.css";
+import API from "../../api/api.js";
 
 type historyItem = {
   name: string;
   desc?: string;
 };
 
-const historyItems: historyItem[] = [
-  {
-    name: "Русаков Алексей Михайлович",
-    desc: "Директор проекта Персона",
-  },
-  {
-    name: "Филатов Вячеслав Валерьевич", 
-    desc: "Оченьоченьоченьоченьдлинноеслово Алгоритмист программист преподаватель",
-  },
-  {
-    name: "Болотов Александр",
-    desc: "Фронтендер",
-  },
-  { name: "Филатов Вячеслав" },
-  { name: "Болотов Александр" },
-  { name: "Филатов Вячеслав" },
-  { name: "Болотов Александр" },
-  { name: "Филатов Вячеслав" },
-  { name: "Болотов Александр" },
-  { name: "Филатов Вячеслав" },
-];
+// const historyItems: historyItem[] = [
+//   {
+//     name: "Русаков Алексей Михайлович",
+//     desc: "Директор проекта Персона",
+//   },
+//   {
+//     name: "Филатов Вячеслав Валерьевич", 
+//     desc: "Оченьоченьоченьоченьдлинноеслово Алгоритмист программист преподаватель",
+//   },
+//   {
+//     name: "Болотов Александр",
+//     desc: "Фронтендер",
+//   },
+//   { name: "Филатов Вячеслав" },
+//   { name: "Болотов Александр" },
+//   { name: "Филатов Вячеслав" },
+//   { name: "Болотов Александр" },
+//   { name: "Филатов Вячеслав" },
+//   { name: "Болотов Александр" },
+//   { name: "Филатов Вячеслав" },
+// ];
+
+let lastMsgID = 0;
+
+// {"capture":"http://127.0.0.1:5000\\capture\\1661797039825_5371237021.jpg","milliseconds":1661797039825,"name":"\u0420\u0443\u0441\u0430\u043a\u043e\u0432 \u0410\u043b\u0435\u043a\u0441\u0435\u0439 \u041c\u0438\u0445\u0430\u0439\u043b\u043e\u0432\u0438\u0447","name_id":4,"photo":"http://127.0.0.1:5000/photo/Rusakov.jpg","timestr":"2022-08-29 21:17:19.825000              "}
 
 function MainLayout() {
+  const [historyItems, setHistoryItems] = useState([]);
+  const [person, setPerson] = useState({
+    name: null,
+    photo: null,
+    cadr: null,
+    desc: null
+  });
+  useEffect(() => {
+    const intervalCtx = setInterval(async () => {
+      try {
+        const msg = await API.getDash(lastMsgID);
+        if (typeof msg === typeof {}) {
+          const {name, photo, capture, desc} = msg;
+          setHistoryItems([...historyItems, {name}]);
+          setPerson({
+            name,
+            photo,
+            cadr: capture,
+            desc
+          });
+          lastMsgID++;
+        }
+      } catch (e) {}
+    }, 2000);
+    return () => {
+      clearInterval(intervalCtx);
+    }
+  });
   return (
     <main className={styles.root}>
       <div className={styles.mainSection}>
         <LiveStream className={styles.videoBlock} />
         <DetectionCard
           className={styles.detectionBlock}
-          personPhoto="http://localhost:5000/photo/Rusakov.jpg"
-          cadr="http://localhost:5000/photo/test.jpg"
-          name="Русаков Алексей Михайлович"
-          desc="Директор проекта Персона"
+          personPhoto={person.photo}
+          cadr={person.cadr}
+          name={person.name}
+          desc={person.desc}
         />
       </div>
       <section className={styles.spinnerBlock}>
